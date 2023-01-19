3dba4d960e38eeb29ddc4da51ba35b6ca4a6eac2
AVANI JAIN <79253951+avanijain311@users.noreply.github.com>
2023-01-19 19:20:20 +0530

    Update playPigGame.java

--- playPigGame.java
+++ playPigGame.java
@@ -1,44 +1,48 @@
-package com.monocept.test;
+package com.monocept.work;
 
 import java.util.Random;
 import java.util.Scanner;
 
 public class playPigGame {
-	Scanner s = new Scanner(System.in);
-	int turn= 0;
-	int rand= 0;
-	int points= 0;
-	int totalPoints=0;
-	{
-	while(totalPoints < 21) {
-		System.out.println("Your Wish : Roll or Hold ! ");
-		String w =s.next(); 
-		if(w.equalsIgnoreCase("r")) {
-			Random rand = new Random();
-			int dieResult = rand.nextInt(6) +1;
-			System.out.println("Die result = " + dieResult);
-			if(dieResult==1) {
-				points=0;
-				totalPoints=0;
-				turn++;
-				System.out.println(" -- Game over --");
-				break;
-			}
-			points = points +dieResult;
-		}
-		while(totalPoints > 21) {
-			System.out.println("Points is " + points);
-			System.out.println("Toatal Points is " + totalPoints);
-			break;
-		}
-		totalPoints =totalPoints + points;
-		System.out.println("Points is " + points);
-		System.out.println("Toatal Points is " + totalPoints);
-		turn++;
-		points = 0;
-		break;
-		
-	}
- }    
-}
+	public static void main(String[] args) {
+		int points = 0;
+		int totalPoints = 0;
+		int totalTurns=0;
+		Scanner s = new Scanner(System.in);
 
+		while (totalPoints <= 20) {
+			System.out.println("Your Wish Roll (r) or Hold (h) ");
+			String wish = s.next();
+			if (wish.equalsIgnoreCase("r")) {
+				Random rand = new Random();
+				int dieResult = rand.nextInt(6) + 1;
+				System.out.println("Die result = " + dieResult);
+				totalTurns++;
+				if (dieResult == 1) {
+					points = 0;
+					totalPoints = 0;
+					//totalTurns = totalTurns-1;
+					System.out.println("Points : " + points);
+					System.out.println("Your Total turns " + totalTurns);
+					System.out.println(" ");
+					System.out.println(" --Turn  over --");
+					break;
+				} else {
+					points = points + dieResult;
+					System.out.println("Points :  " + points);
+				}
+				if (points>=20) {
+					System.out.println("Your Total turns " + totalTurns);
+					break; 
+				} else if (wish.equalsIgnoreCase("h")) {
+					totalPoints = totalPoints + points;
+					System.out.println("You hold and your Total turns is " + totalPoints);
+					totalTurns++;
+					points = 0;
+					break;
+				}
+
+			}
+		}
+	}
+}
